12915dded439720f920179c5b2a3c49bf1e8f183
Oryan Hachamoff <oryanhach95@gmail.com>
2023-07-05 21:23:40 +0300

    commit for first delivery

--- apps/keep/cmps/KeepMenu.js
+++ apps/keep/cmps/KeepMenu.js
@@ -1,8 +1,19 @@
 export default {
     template: `
             <section class="keep-menu">
-                <input class="create-note-input" type="text" placeholder="What's on your mind...">
-                <button class="add-note-btn">Add note</button>
+                <input v-model="note" class="create-note-input" type="text">
+                <button @click="addNote()" class="add-note-btn">Add note</button> 
             </section>
-            `
+            `,
+    data() {
+        return {
+            note: "What's on your mind...",
+        }
+    },
+    methods: {
+        addNote() {
+            console.log('Note added: ', this.note)
+            $emit('addNote', this.note)
+        },
+    }
 }
